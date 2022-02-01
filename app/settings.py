521862aacaa4086845f36d785d094aa71e2e36ea
salaheddine el azzaouy <salaheddine.elazzaouy@outlook.fr>
2022-02-01 16:11:00 +0000

    add alowed hostst

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jyp^fvjt0sr)_wm9diqw35*k6mw!3*=wl@8r2$hwhsfcu%-n-5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
