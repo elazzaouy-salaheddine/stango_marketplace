02d6d36adf43efb8c16b30f39574834f5beb575f
salaheddine el azzaouy <salaheddine.elazzaouy@outlook.fr>
2022-02-28 15:35:37 +0000

    saffsafas

--- comment/migrations/0007_comment_review.py
+++ comment/migrations/0007_comment_review.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='comment',
             name='review',
-            field=models.ForeignKey(default=2, on_delete=django.db.models.deletion.CASCADE, to='product.product'),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to='product.product'),
             preserve_default=False,
         ),
     ]
